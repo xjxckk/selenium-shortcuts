8b1a6062a43e7cba5dc318d02da704970bae4acd
xJxckk <17519346+xjxckk@users.noreply.github.com>
2022-03-23 14:19:57 +0000

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name = 'error-alerts',
-    packages = ['alerts'],
-    version = '2',
-    description = 'Error alerts via Telegram',
-    url = 'https://github.com/xjxckk/error-alerts/',
-    download_url = 'https://github.com/xjxckk/error-alerts/archive/refs/tags/v2.tar.gz',
+    name = 'selenium_shortcuts',
+    packages = ['selenium_shortcuts'],
+    version = '1',
+    description = 'Selenium shortcut functions',
+    url = 'https://github.com/xjxckk/selenium_shortcuts/',
+    download_url = 'https://github.com/xjxckk/selenium_shortcuts/archive/refs/tags/v1.tar.gz',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown'
     )
