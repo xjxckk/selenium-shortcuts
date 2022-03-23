c1ab47fc2ed5eacfb04805fac754687d73414d7e
xJxckk <17519346+xjxckk@users.noreply.github.com>
2022-03-23 14:33:56 +0000

    update

--- selenium_shortcuts/__init__.py
+++ selenium_shortcuts/__init__.py
@@ -3,25 +3,35 @@ class setup:
     def __init__(self, driver):
         self.driver = driver
         
-    def find(self, css_selector, attribute=None, parent=driver):
+    def find(self, css_selector, attribute=None, parent=None):
+        if not parent:
+            parent = self.driver
         element = parent.find_element_by_css_selector(css_selector)
         if attribute:
             attribute = element.get_attribute(attribute)
             return element, attribute
         return element
 
-    def finds(css_selector, parent=driver):
+    def finds(self, css_selector, parent=None):
+        if not parent:
+            parent = self.driver
         elements = parent.find_elements_by_css_selector(css_selector)
         return elements
 
-    def click(css_selector, parent=driver):
+    def click(self, css_selector, parent=None):
+        if not parent:
+            parent = self.driver
         element = parent.find_element_by_css_selector(css_selector)
         element.click()
 
-    def text(css_selector, parent=driver):
+    def text(self, css_selector, parent=None):
+        if not parent:
+            parent = self.driver
         element = parent.find_element_by_css_selector(css_selector)
         return element.text
 
-    def send(css_selector, text, parent=driver):
+    def send(self, css_selector, text, parent=None):
+        if not parent:
+            parent = self.driver
         element = parent.find_element_by_css_selector(css_selector)
         element.send_keys(text)
